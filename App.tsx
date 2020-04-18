4d1c4ef78419cb7efb08e69b8183c60cf8b856ef
longas123 <mateusmontano@gmail.com>
2020-04-18 16:57:36 -0300

    Entradas dos valores

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 import BestFluel from './BestFluel';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <BestFluel gasolina={10} etanol={2} />
+    <View style={styles.container} >
+        <View style={styles.entradas}>
+          <Text style={styles.preco}>Preço Gasolina:</Text>
+          <TextInput placeholder="Gasolina" keyboardType="numeric" style={styles.input}/>
+          <Text style={styles.preco}>Preço Etanol:</Text>
+          <TextInput placeholder="Etanol" keyboardType="numeric" style={styles.input}/>
+        </View> 
+        <View>
+          <TouchableOpacity style={styles.button} onPress={()=>{}}><Text>Calcular</Text></TouchableOpacity>
+        </View>  
     </View>
   );
 }
@@ -15,8 +22,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    height: 80,
+    textAlign: "center",
+    fontSize:30,
+    marginTop: 5,
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: "#FF7F00",
+    padding: 10,
+    borderWidth: 1,
+    marginTop: 10,
+
+  },
+  entradas: {
+   // flexDirection: 'row' ,
+  },
+  preco: {
+    fontSize: 25,
+  },
+  buttonText: {
+    alignSelf: "center",
+    
+  }
 });
