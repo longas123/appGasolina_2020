d43a499750fed4ce70b1b609356181944bfd39e1
longas123 <mateusmontano@gmail.com>
2020-04-19 16:49:28 -0300

    Ajustes nos Inputs, Button, e no style

--- App.tsx
+++ App.tsx
@@ -1,28 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput,  } from 'react-native';
 
 import BestFluel from './BestFluel';
 
-export default function App() {
+
+type Props = {};
+export default class App extends Component<Props>{
+  
+constructor(props){
+  super(props)
+  this.state = {gasolina:0, etanol:0, resultado:0}
+  this.calcular =  this.calcular.bind(this) 
+}
+
+// Aqui vai ser feito o cálculo, não sei como usar essa função n arquivo BestFluel
+calcular(){
+
+}
+
+ render(){
   return (
     <View style={styles.container} >
         <View style={styles.entradas}>
           <Text style={styles.preco}>Preço Gasolina:</Text>
-          <TextInput placeholder="Gasolina" keyboardType="numeric" style={styles.input}/>
+          <TextInput placeholder="Gasolina" keyboardType="numeric" style={styles.input} onChangeText={(gasolina)=>this.setState(gasolina)} />
           <Text style={styles.preco}>Preço Etanol:</Text>
-          <TextInput placeholder="Etanol" keyboardType="numeric" style={styles.input}/>
+          <TextInput placeholder="Etanol" keyboardType="numeric" style={styles.input}  onChangeText={(etanol)=>this.setState(etanol)}/>
         </View> 
         <View>
-          <TouchableOpacity style={styles.button} onPress={()=>{}}><Text>Calcular</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.calcular}><Text>Calcular</Text></TouchableOpacity>
         </View>  
     </View>
   );
+ }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFFAFA",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -38,7 +53,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
     marginTop: 10,
-
+    fontSize: 30,
+    borderRadius: 60,
   },
   entradas: {
    // flexDirection: 'row' ,
@@ -47,7 +63,6 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
   buttonText: {
-    alignSelf: "center",
-    
+    alignSelf: "center",  
   }
 });
